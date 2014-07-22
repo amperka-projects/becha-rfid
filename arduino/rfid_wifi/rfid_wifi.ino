d3315df686e3418358ecdfbe00595dda5fcc6b61
acosinwork <acosinwork@yandex.ru>
2014-07-22 17:05:03 +0400

    badWork

--- arduino/rfid_wifi/rfid_wifi.ino
+++ arduino/rfid_wifi/rfid_wifi.ino
@@ -4,22 +4,28 @@ We use Arduino Leonardo
 
 #include <SoftwareSerial.h>
 
-#define MY_SSID            "Amperka.ru"
-#define MY_PASSWORD        "password"
-#define MY_WIFI_CHANNEL    "6"
+#define WIFI_DEBUG
 
-#define MY_SECURITY_WPA_TKIP    "2"
-#define MY_SECURITY_WPA_MIXED   "3"
-#define MY_SECURITY_WPA2_AES    "4"
+// Wi-Fi preference
+#define COMMAND_MODE_ENRTERING  "$$$"
+#define COMMAND_MODE_DELAY      500
 
-#define JOIN_MODE_MANUAL   "0"
-#define JOIN_MODE_AUTO     "1"
+#define MY_SSID                 "Amperka.ru"
+#define MY_PASSWORD             "mega2560"
+#define AUTO_WIFI_CHANNEL       "0"
 
-#define UDP_HOST_IP        "192.168.10.159"
-#define UDP_REMOTE_PORT    "2014"
-#define UDP_LOCAL_PORT     "2014"
+#define SECURITY_WPA_TKIP       "2"
+#define SECURITY_WPA_MIXED      "3"
+#define SECURITY_WPA2_AES       "4"
+#define SECURITY                SECURITY_WPA_MIXED
 
-#define LED_PIN 13
+#define JOIN_MODE_MANUAL        "0"
+#define JOIN_MODE_AUTO          "1"
+#define JOIN_MODE               JOIN_MODE_AUTO
+
+#define UDP_HOST_IP             "192.168.10.159"
+#define UDP_REMOTE_PORT         "2014"
+#define UDP_LOCAL_PORT          "2014"
 
 SoftwareSerial mySerial(10, 11);
 
@@ -40,15 +46,27 @@ char sendData[RFID_DATA_LEN];
 unsigned long timePoint = 0;
 unsigned long timeClearPoint = 0;
 
-void setup() 
+#define LED_PIN 13
+
+void setup()
 {
   Serial1.begin(9600);
   mySerial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
+
+#ifdef WIFI_DEBUG
+  Serial.begin(9600);
+  while (!Serial.available())
+  {
+    delay(10);
+  }
+#endif
+
+  setupWiFly();
 }
 
 
-void loop() 
+void loop()
 {
   while (Serial1.available())
     Serial1.read(); //clear buffer
@@ -75,7 +93,7 @@ void loop()
     digitalWrite(LED_PIN, !digitalRead(LED_PIN));
   }
 
-  if (millis() - timeClearPoint > REPEAT_DELAY) { 
+  if (millis() - timeClearPoint > REPEAT_DELAY) {
     // clear stored data after to repeat send
     memset(sendData, 0, RFID_DATA_LEN);
     timeClearPoint = millis();
@@ -95,7 +113,7 @@ void readRfidData()
 
   int i = 0;
   while (mySerial.available() && i < RFID_DATA_LEN)
-      incomingData[i++] = mySerial.read();
+    incomingData[i++] = mySerial.read();
 
   while (mySerial.available())
     mySerial.read(); //clear buffer
@@ -104,31 +122,35 @@ void readRfidData()
 
 void setupWiFly()
 {
-  delay(1000);
+//  delay(1000);
 
   readWiFlyMessages();
 
-// command mode
-  sendWiFlyPreference("$$$");
+  // command mode
+  sendWiFlyPreference(COMMAND_MODE_ENRTERING);
 
-// wi-fi settings
-  sendWiFlyPreference("set wlan join "    JOIN_MODE_AUTO       "\n");
-  sendWiFlyPreference("set wlan ssid "    MY_SSID              "\n");
-  sendWiFlyPreference("set wlan auth "    MY_SECURITY_WPA_TKIP "\n");
-  sendWiFlyPreference("set wlan phrase "  MY_PASSWORD          "\n");
-  sendWiFlyPreference("set wlan channel " MY_WIFI_CHANNEL      "\n");
+  // wait command mode
+  delay(COMMAND_MODE_DELAY);
 
-// network settings
+  // wi-fi settings
+  sendWiFlyPreference("set wlan ssid "    MY_SSID              "\r");
+  sendWiFlyPreference("set wlan phrase "  MY_PASSWORD          "\r");
+
+  sendWiFlyPreference("set wlan join "    JOIN_MODE            "\r");
+  sendWiFlyPreference("set wlan auth "    SECURITY             "\r");
+  sendWiFlyPreference("set wlan channel " AUTO_WIFI_CHANNEL    "\r");
+
+  // network settings
   sendWiFlyPreference("set ip proto 1");                      // enable UDP as the protocol
-  sendWiFlyPreference("set ip host "   UDP_HOST_IP     "\n"); // set the IP address of remote host
-  sendWiFlyPreference("set ip remote " UDP_REMOTE_PORT "\n"); // set the remote port number on which the host is listening
-  sendWiFlyPreference("set ip local "  UDP_LOCAL_PORT  "\n"); // set the port number on which the WiFly module will listen
+  sendWiFlyPreference("set ip host "   UDP_HOST_IP     "\r"); // set the IP address of remote host
+  sendWiFlyPreference("set ip remote " UDP_REMOTE_PORT "\r"); // set the remote port number on which the host is listening
+  sendWiFlyPreference("set ip local "  UDP_LOCAL_PORT  "\r"); // set the port number on which the WiFly module will listen
 
-// save new settings
-  sendWiFlyPreference("save\n");                                // saves the settings in config file
-  sendWiFlyPreference("reboot\n");                              // reboots the module so that the above settings take effect
+  // save new settings
+  sendWiFlyPreference("save\r");                                // saves the settings in config file
+  sendWiFlyPreference("reboot\r");                              // reboots the module so that the above settings take effect
 
-  delay(1000);
+//  delay(1000);
 
   readWiFlyMessages();
 }
@@ -136,15 +158,16 @@ void setupWiFly()
 void sendWiFlyPreference(const char *preferenceText)
 {
   Serial1.print(preferenceText);
+//  Serial1.flush();
 
   if (Serial) {
     Serial.println("");
     Serial.print("> ");
-    Serial.println(preferenceText);
+//    Serial.println(preferenceText);
   }
 
   while (!Serial1.available())
-    ;
+    delay(20);
 
   delay(10);
 
@@ -154,6 +177,7 @@ void sendWiFlyPreference(const char *preferenceText)
 void readWiFlyMessages()
 {
   while (Serial1.available()) {
+    delay(10);
     if (Serial)
       Serial.write(Serial1.read());
     else
